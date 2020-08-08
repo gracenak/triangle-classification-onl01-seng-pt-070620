3c9aab9221da2e6c3203b0f1574634538aca62a7
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-08 05:02:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,11 +9,19 @@ attr_accessor :equilateral, :isosceles, :scalene
     @side_3 = side_3
   end
   
+  it 'knows that triangles violating triangle inequality are illegal 2' do
+    expect{Triangle.new(2, 4, 2).kind}.to raise_error(Triangle::TriangleError)
+  end
+
+  it 'knows that triangles violating triangle inequality are illegal 3' do
+    expect{Triangle.new(7, 3, 2).kind}.to raise_error(Triangle::TriangleError)
+  end
+  
   def kind
     if @side_1 <= 0 && @side_2 <= 0 && @side_3 <= 0 
       raise TriangleError
        
-      elsif (@side_1 + @side_2) < @side_3 || (@side_2 + @side_3) < @side_1 || (@side_3 + @side_1) < @side_2
+      elsif (@side_1 + @side_2) <= @side_3 || (@side_2 + @side_3) <= @side_1 || (@side_3 + @side_1) <= @side_2
         raise TriangleError
       else 
         
