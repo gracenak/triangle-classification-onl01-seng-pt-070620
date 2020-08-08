294763c9060124a6f2bd65f240cc13888b811fc3
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-08 03:58:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -25,7 +25,6 @@ class Triangle
       elsif (@side_1 + @side_2) < @side_3 || (@side_2 + @side_3) < @side_1 || (@side_3 + @side_1) < @side_2
         raise TriangleError
       end
-      end
     end
   end
   
