e7bf174fb652440cf93853074f48d3e6c7b80552
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-08 03:45:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
     if @side_1 < 0 && @side_2 < 0 && @side_3 < 0
        raise TriangleError
        
-    elsif sum(@side_1 + @side_2) < @side_3 || sum(@side_2 + @side_3) < @side_1 || sum(@side_3 + @side_1) < @side_2
+    elsif (@side_1 + @side_2) < @side_3 || (@side_2 + @side_3) < @side_1 || (@side_3 + @side_1) < @side_2
       raise TriangleError
     end
      
