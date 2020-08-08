62f3332ee1528421336e223d8295f58fb9485354
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-08 03:40:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,40 +20,15 @@ class Triangle
      
       if @side_1 == @side_2 && @side_2 == @side_3 
         :equilateral
-      elsif @side_1 == @side_2  || @side_2 == @side_3 || @side_1 == @side_3 
+      elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3 
         :isosceles
       elsif @side_1 != @side_2 && @side_2 != @@side_3 && @side_3 != @side_1
         :scalene
-      
-    
-     #elsif equilateral.all.detect {|x| x = y && y = z && x = z|}
-#       self.equilateral
-  
-#     elsif 
-    
-#     isosceles.all.detect {|x| x < y && x = z}
-#       self.isosceles
-      
-#     elsif
-#     scalene.all.detect {|x| x > y && y > z && x > z}
-#     self.scalene
+      end
+    end
   end
-
- end
- 
-# if x < 0 && y < 0 && z < 0  
-#     raise TriangleError
-    
-    
-#   # class TriangleError < StandardError
-#   #   side_1 = a > 0
-#   #   side_2 = b > 0
-#   #   side_3 = c > 0
-    
-#   #   if sum(a + b) > c || sum(b + c) > a || sum(a + c) > c ? true : false
+end
       
-      
-    
     
   
 equilateral = Triangle.new(2, 2, 2).kind
