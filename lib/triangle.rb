f9acb5c3426aa8b27d6faed4a12ca35829f6318c
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-08 04:38:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,7 +18,7 @@ attr_accessor :equilateral, :isosceles, :scalene
         :scalene
       else
         
-      if @side_1 <= 0 && @side_2 <= 0 && @side_3 <= 0 ? true : false
+      if @side_1 <= 0 && @side_2 <= 0 && @side_3 <= 0 == true
         raise RangeError
        
         elsif (@side_1 + @side_2) < @side_3 || (@side_2 + @side_3) < @side_1 || (@side_3 + @side_1) < @side_2
