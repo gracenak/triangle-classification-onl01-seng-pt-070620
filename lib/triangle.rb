24c5851a55d918aa7f8bf83a54a73c146c817ff2
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-08 05:00:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,21 +10,19 @@ attr_accessor :equilateral, :isosceles, :scalene
   end
   
   def kind
-          if @side_1 <= 0 && @side_2 <= 0 && @side_3 <= 0 
-        raise TriangleError
+    if @side_1 <= 0 && @side_2 <= 0 && @side_3 <= 0 
+      raise TriangleError
        
-        elsif (@side_1 + @side_2) < @side_3 || (@side_2 + @side_3) < @side_1 || (@side_3 + @side_1) < @side_2
+      elsif (@side_1 + @side_2) < @side_3 || (@side_2 + @side_3) < @side_1 || (@side_3 + @side_1) < @side_2
         raise TriangleError
       else 
         
-    if @side_1 == @side_2 && @side_2 == @side_3
-      :equilateral
-      elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3 
-        :isosceles
-      elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_3 != @side_1
-        :scalene
-        
-
+      if @side_1 == @side_2 && @side_2 == @side_3
+        :equilateral
+        elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3 
+          :isosceles
+        elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_3 != @side_1
+          :scalene
         end
       end
     end
