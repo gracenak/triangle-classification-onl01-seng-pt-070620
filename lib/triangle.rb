70d161bc5528b27a9ad85edbe86fa1d3ed2e7a07
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-08 02:50:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,15 +3,15 @@ class Triangle
   
   attr_accessor :equilateral, :isosceles, :scalene
   
-  def initialize(side_1, side_2, side_3)
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
   end
   
   def kind
     binding.pry
-   if  x > 0 && y > 0 && z > 0
+     if side_1 > 0 && side_2 > 0 && side_3 > 0
      sum(x + y) > z || sum(y + z) > x || sum(z + x) > y
      return true 
     
