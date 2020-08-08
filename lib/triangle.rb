d3b4a93d533f27dd8a6b7fa9548b5ec191048e3c
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-08 03:38:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,6 +23,7 @@ class Triangle
       elsif @side_1 == @side_2  || @side_2 == @side_3 || @side_1 == @side_3 
         :isosceles
       elsif @side_1 != @side_2 && @side_2 != @@side_3 && @side_3 != @side_1
+        :scalene
       
     
      #elsif equilateral.all.detect {|x| x = y && y = z && x = z|}
