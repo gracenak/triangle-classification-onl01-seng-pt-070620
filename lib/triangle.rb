0d07c1aeb6181b71d8207172aa13dda28676042d
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-08 04:27:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,9 +20,9 @@ class Triangle
     else
         
       if @side_1 <= 0 && @side_2 <= 0 && @side_3 <= 0
-          raise TriangleError
+        raise TriangleError
        
-        elsif (@side_1 + @side_2) < @side_3 || (@side_2 + @side_3) < @side_1 || (@side_3 + @side_1) < @side_2
+      elsif (@side_1 + @side_2) < @side_3 || (@side_2 + @side_3) < @side_1 || (@side_3 + @side_1) < @side_2
         raise TriangleError
         end
       end
