f707511cbd2cd69e044cb7aeb523503e16a9c9db
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-08 03:42:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,16 +18,19 @@ class Triangle
       raise TriangleError
      end
      
-      if @side_1 == @side_2 && @side_2 == @side_3 
+        if @side_1 == @side_2 && @side_2 == @side_3 
         :equilateral
-      elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3 
+        elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3 
         :isosceles
-      elsif @side_1 != @side_2 && @side_2 != @@side_3 && @side_3 != @side_1
+        elsif @side_1 != @side_2 && @side_2 != @@side_3 && @side_3 != @side_1
         :scalene
+        end
       end
     end
   end
-      
+  
+  
+end 
     
   
 equilateral = Triangle.new(2, 2, 2).kind
