0272590e7cbf2069c6b4f64d0369ac09e36d6dda
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-08 04:18:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,7 +3,7 @@ class Triangle
   
   attr_accessor :equilateral, :isosceles, :scalene
   
-  def initialize(@side_1, @side_2, @side_3)
+  def initialize(side_1, side_2, side_3)
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
