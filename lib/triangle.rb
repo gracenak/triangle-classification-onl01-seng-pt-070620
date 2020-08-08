651ffbd89b3c2b0a8ef3dcca500a3212ef96cd04
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-08 04:01:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,20 +13,16 @@ class Triangle
     if @side_1 <= 0 && @side_2 <= 0 && @side_3 <= 0
       raise TriangleError
        
-    elsif (@side_1 + @side_2) < @side_3 || (@side_2 + @side_3) < @side_1 || (@side_3 + @side_1) < @side_2
+      elsif (@side_1 + @side_2) < @side_3 || (@side_2 + @side_3) < @side_1 || (@side_3 + @side_1) < @side_2
         raise TriangleError
         
-        
     
-    if @side_1 == @side_2 && @side_2 == @side_3 
-      :equilateral
-    elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3 
-      :isosceles
-    elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_3 != @side_1
-      :scalene
-    end
-        
-
+      elsif @side_1 == @side_2 && @side_2 == @side_3 
+        :equilateral
+      elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3 
+        :isosceles
+      elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_3 != @side_1
+        :scalene
       end
     end
   end
