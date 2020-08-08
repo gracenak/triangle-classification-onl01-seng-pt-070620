b0bb98bca927355e14952046fad64f70ad3f4f58
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-08 03:30:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,7 +20,7 @@ class Triangle
      
       if @side_1 == @side_2 && @side_2 == @side_3 
         :equilateral
-      elsif @side_1 < @side_2 && @side_1 == @side_3 || @side_2 == @side_3
+      elsif @side_1 == @side_2 || @side_2 == @side_3
         :isosceles
       elsif @side_1 > @side_2 && @side_2 > @@side_3 && @side_3 > @side_1
       
