89aeb56847877114dec95d09f4e65af623b2e98a
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-08 03:58:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,6 +17,7 @@ class Triangle
       :isosceles
     elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_3 != @side_1
       :scalene
+    end
         
       if @side_1 <= 0 && @side_2 <= 0 && @side_3 <= 0
         raise TriangleError
