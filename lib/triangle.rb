3f97d39bb0f71fe44a49cfcfc42e63eeb972eae7
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-08 03:44:12 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
   
   def kind
     
-    if @side_1 =< 0 && @side_2 =< 0 && @side_3 =< 0
+    if @side_1 < 0 && @side_2 < 0 && @side_3 < 0
        raise TriangleError
        
     elsif sum(@side_1 + @side_2) < @side_3 || sum(@side_2 + @side_3) < @side_1 || sum(@side_3 + @side_1) < @side_2
@@ -26,7 +26,7 @@ class Triangle
         :scalene
         
       end
-    
+    end
   end
   
   
